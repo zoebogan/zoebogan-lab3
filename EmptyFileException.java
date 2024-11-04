11ecb7d3475b3282a3723362700ad63cce7991fb
zoebogan <zoe.bogan@gwu.edu>
2024-11-03 22:26:51 -0500

    empty file

--- EmptyFileException.java
+++ EmptyFileException.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 public class EmptyFileException extends IOException {
 
-
-
+    public EmptyFileException(String errorMessage) {
+        super(errorMessage);
+    }
 }
